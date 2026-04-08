9c052504ba1d071a485bfcb4a4b3e7c8f2dd4f1a
John Doe <christianjin123@gmail.com>
2026-04-08 17:14:09 +0200

    test

--- good_game_db.php
+++ good_game_db.php
@@ -1,31 +1,13 @@
 <?php
-mysqli_report(MYSQLI_REPORT_OFF);
-
+$servername = "db";
 $username = "root";
+$passowrd = "root";
 $dbname = "good_game";
 
-// Détection de l'environnement
-if (gethostbyname('db') !== 'db') {
-    // Docker
-    $servername = "db";
-    $password = "root";
-} else {
-    // XAMPP
-    $servername = "localhost";
-    $password = "";
-}
+$conn = new mysqli($servername, $username, $passowrd, $dbname);
+$conn->set_charset("utf8mb4");
 
-// Tentative de connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// GESTION DES ERREURS
-if ($conn->connect_errno) {
-    // Erreur 1049 = La base de données n'existe pas
-    if ($conn->connect_errno == 1049) {
-        header("Location: ini.php");
-        exit();
-    } else {
-        // Autre erreur
-        die("Erreur de connexion : " . $conn->connect_error);
-    }
+if ($conn->connect_errno == 1049) {
+    header("Location: ini.php");
+    exit();
 }
